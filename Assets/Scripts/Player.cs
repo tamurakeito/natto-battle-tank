5085d9dbfde081af2d4cbcc60ab0e335a2be17a7
田村慧人 <tamurakeito@tamuratoshininnoMacBook-Air.local>
2022-08-01 17:22:36 +0900

    タンクのインポートと移動座標の修正

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,19 +7,13 @@ public class Player : MonoBehaviour
     float x, z;
     float speed = 1.1f;
 
-    public GameObject cam;
-    Quaternion cameraRot, characterRot;
+    Quaternion characterRot;
     float Xsensityvity = 3f, Ysensityvity = 3f;
     
     bool cursorLock = true;
 
-    //変数の宣言(角度の制限用)
-    float minX = -90f, maxX = 90f;
-
-    // Start is called before the first frame update
     void Start()
     {
-        cameraRot = cam.transform.localRotation;
         characterRot = transform.localRotation;
     }
 
@@ -29,13 +23,7 @@ public class Player : MonoBehaviour
         float xRot = Input.GetAxis("Mouse X") * Ysensityvity;
         float yRot = Input.GetAxis("Mouse Y") * Xsensityvity;
 
-        cameraRot *= Quaternion.Euler(-yRot, 0, 0);
         characterRot *= Quaternion.Euler(0, xRot, 0);
-
-        //Updateの中で作成した関数を呼ぶ
-        cameraRot = ClampRotation(cameraRot);
-
-        cam.transform.localRotation = cameraRot;
         transform.localRotation = characterRot;
 
 
@@ -50,9 +38,7 @@ public class Player : MonoBehaviour
         x = Input.GetAxisRaw("Horizontal") * speed;
         z = Input.GetAxisRaw("Vertical") * speed;
 
-        //transform.position += new Vector3(x,0,z);
-
-        transform.position += cam.transform.forward * z + cam.transform.right * x;
+        transform.position += transform.forward * z + transform.right * x;
     }
 
 
@@ -77,25 +63,5 @@ public class Player : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
         }
     }
-    
-    //角度制限関数の作成
-    public Quaternion ClampRotation(Quaternion q)
-    {
-        //q = x,y,z,w (x,y,zはベクトル（量と向き）：wはスカラー（座標とは無関係の量）)
-
-        q.x /= q.w;
-        q.y /= q.w;
-        q.z /= q.w;
-        q.w = 1f;
-
-        float angleX = Mathf.Atan(q.x) * Mathf.Rad2Deg * 2f;
-
-        angleX = Mathf.Clamp(angleX,minX,maxX);
-
-        q.x = Mathf.Tan(angleX * Mathf.Deg2Rad * 0.5f);
-
-        return q;
-    }
-
 
 }
